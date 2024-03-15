64556992aa5f65297b90681485bf567e9696b767
dayoungs <dyshin0920@gmail.com>
2024-03-15 16:09:59 +0900

    gpt 디렉토리 생성

---
+++ src/main/java/capston/psylog/gpt/dto/Message.java
@@ -0,0 +1,13 @@
+package capston.psylog.gpt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String role;
+    private String content;
+}
